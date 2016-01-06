a872631d9ecc885644b3f8b0980e9da30cf29671
SONG Peng <eyakcn@gmail.com>
2016-01-06 21:40:45 +0800

    more loaders, and run npm in mvn command

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,42 @@
 var path = require('path');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 module.exports = {
-  context: __dirname + '/src/main/js',
+  context: path.join(__dirname, 'src/main/js'),
   entry: ['./app.js', './report.js'],
   devtool: 'sourcemaps',
-  cache: false,
+  cache: true,
   debug: true,
   output: {
-    path: __dirname,
-    filename: 'src/main/resources/static/bundle.js'
+    path: path.join(__dirname, 'src/main/webapp'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
-      {
-        test: path.join(__dirname, 'src/main/js'),
-        loader: 'babel'
-      },
-      {
-        test: path.join(__dirname, 'src/main/js'),
-        loader: 'eslint-loader'
-      }
+      {test: path.join(__dirname, 'src/main/js'), loader: 'babel'},
+      {test: path.join(__dirname, 'src/main/js'), loader: 'eslint'},
+
+      {test: /\.jsx$/, loader: 'babel'},
+      {test: /\.less$/, loader: 'style!css!less'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.css$/, loader: 'style!css'},
+
+      // **IMPORTANT** This is needed so that each bootstrap js file required by
+      // bootstrap-webpack has access to the jQuery object
+      {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
+      {test: /[\/]angular\.js$/, loader: "exports?window.angular"},
+
+      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+      // loads bootstrap's css.
+      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
     ]
-  }
+  },
+  plugins: [
+    new ngAnnotatePlugin({
+      add: true
+    })
+  ]
 };
